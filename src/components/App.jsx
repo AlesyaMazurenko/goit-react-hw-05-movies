e8c61c6904e32aec038d03c18698b5113584a937
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-11-07 22:55:07 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage/HomePage';
+import Movies from '../pages/movies/Movies';
+import NavBar from './NavBar/NavBar';
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<NavBar />} >
+          <Route path="home" element={<HomePage />} />
+          <Route path="/movies" element={<Movies />} >
+            <Route path="/movies:id" element={<div>Movie Details</div>} >
+              <Route path="cast" element={<div>Cast</div>} />
+              <Route path="reviews" element={<div>Reviews</div>} />
+            </Route>
+          </Route>
+        </Route>
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </div> 
+  )
+}
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
