6f65289a9bd335f2e9600adfcdb39af76fe107f3
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-11-15 02:33:07 +0100

    o=ss

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,39 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import { Layout } from '../components/Layout/Layout';
+import { lazy, Suspense } from 'react';
+// import MoviesId from '../pages/moviesId/MoviesId';
 import HomePage from '../pages/HomePage/HomePage';
-import Movies from '../pages/movies/Movies';
-import NavBar from './NavBar/NavBar';
-import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+// import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+// import { MovieDetails } from 'pages/MovieDetails/MovieDetails';
+// import {Cast} from 'pages/MovieDetails/Cast/Cast'
+// import Rewievs from 'pages/MovieDetails/Review/Review';
+// import Movie from 'pages/Movie/Movie';
+
+const Movie = lazy(() => import('pages/Movie/Movie'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('pages/MovieDetails/Cast/Cast'));
+const Reviews = lazy(() => import('pages/MovieDetails/Review/Review'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
 
 export const App = () => {
   return (
     <div className='App'>
       <Routes>
-        <Route path="/" element={<NavBar />} >
-          <Route path="home" element={<HomePage />} />
-          <Route path="/movies" element={<Movies />} >
-            <Route path="/movies:id" element={<div>Movie Details</div>} >
-              <Route path="cast" element={<div>Cast</div>} />
-              <Route path="reviews" element={<div>Reviews</div>} />
-            </Route>
+        <Route path="/" element={<Layout /> }> 
+          <Route index element={<HomePage />} />
+          <Route path="movies" element={<Suspense><Movie /></Suspense> }/>
+            <Route path="movies/:id" element={<Suspense><MovieDetails /></Suspense>} >
+              
+            <Route path="cast" element={<Suspense><Cast /></Suspense>} />
+              <Route path="reviews" element={<Suspense><Reviews /></Suspense>} />
           </Route>
+          
         </Route>
-        <Route path='*' element={<NotFoundPage />} />
+       <Route path='*' element={<Suspense><NotFoundPage /></Suspense>} />
       </Routes>
+    <Outlet />
     </div> 
   )
 }
 
-
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
